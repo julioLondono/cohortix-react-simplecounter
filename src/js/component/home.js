5493401fcfdf09864b724e7f141a40ce4a232f70
Julio Londono <juliolondonogarcia@gmail.com>
2019-05-14 15:41:48 +0000

    Components before the counter calculation

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,25 +1,18 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { Number } from "./number.js";
 
 //create your first component
 export class Home extends React.Component {
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
+			<div className="flex-container">
+				<i className="far fa-clock digit" />
+				<Number number="6" />
+				<Number number="5" />
+				<Number number="4" />
+				<Number number="3" />
+				<Number number="2" />
+				<Number number="1" />
 			</div>
 		);
 	}
