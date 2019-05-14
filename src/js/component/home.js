ebb361bb7cebf9bf8f5651026d903e973d9cbee8
Julio Londono <juliolondonogarcia@gmail.com>
2019-05-14 20:07:24 +0000

    Final version

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,16 +3,35 @@ import { Number } from "./number.js";
 
 //create your first component
 export class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { counter: 0 };
+	}
+
+	componentDidMount() {
+		this.timerID = setInterval(() => this.tick(), 100);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+	tick() {
+		this.setState({
+			counter: this.state.counter + 1
+		});
+	}
+
 	render() {
 		return (
 			<div className="flex-container">
 				<i className="far fa-clock digit" />
-				<Number number="6" />
-				<Number number="5" />
-				<Number number="4" />
-				<Number number="3" />
-				<Number number="2" />
-				<Number number="1" />
+				<Number numero={Math.floor(this.state.counter / 100000) % 10} />
+				<Number numero={Math.floor(this.state.counter / 10000) % 10} />
+				<Number numero={Math.floor(this.state.counter / 1000) % 10} />
+				<Number numero={Math.floor(this.state.counter / 100) % 10} />
+				<Number numero={Math.floor(this.state.counter / 10) % 10} />
+				<Number numero={this.state.counter % 10} />
 			</div>
 		);
 	}
